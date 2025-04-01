883bdb9d3964379380cea9703caaa6ffe00e46d7
Josue Guevara <tv.e5150@gmail.com>
2025-03-31 18:01:27 -0600

    Bingo!

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
     // Enable if we need to read markdown/DITA files at build time
     serverComponentsExternalPackages: ['gray-matter'],
   },
+  // Add support for importing SVGs as React components
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 export default nextConfig; 
